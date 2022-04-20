70eb4d95a404bc9751ab5cabf5f639abc3e4ec66
Your Name <you@example.com>
2022-04-20 15:33:16 +0800

    first 20

--- deploy-circleci.sh
+++ deploy-circleci.sh
@@ -1,27 +1,30 @@
+# deploy.sh
 #!/usr/bin/env sh
 
-# 终止一个错误
+# abort on errors
 set -e
 
-git config --global user.email "you@example.com"
-git config --global user.name "your name"
-
-
-# 构建
+# build
 npm run build
 
-# 进入生成的构建文件夹
-cd dev/.vuepress/dist
+# navigate into the build output directory
+cd dist
 
-# 如果你是要部署到自定义域名
+# if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
 
 git init
+git config --global user.email "xxxx@126.com"
+git config --global user.name "用户名"
 git add -A
-git commit -m 'deploy'
+git commit -m 'deploy [ci skip]'
 
-echo "before push"
-# 如果你想要部署到 https://<USERNAME>.github.io
-git push -f git@github.com:<ccccl-132>/<ccccl-132>.github.io.git master
-echo "after push"
-# cd -
+# if you are deploying to https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:<ccccl-132>/<ccccl-132>.git master:gh-pages
+
+rm -rf dist
+cd -
+
